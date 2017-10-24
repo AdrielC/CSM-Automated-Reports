c50beb876a576999b60c01f548a96e2ce3df655a
Adriel Casellas <adrielcasellas@gmail.com>
2017-10-24 09:01:16 -0600

    fixed attendee report append

---
+++ setup.py
@@ -0,0 +1,6 @@
+### BYU Bridge API username and token
+
+USERNAME = "bccbrand"
+TOKEN = "phFJOCLh7+6tXa/CuP4uTbgZDwlBjQyORbc/sI1C6/yLylYouRzjLPkM/EMiKennN0tlp0TkZGMnQTLq1QOb+XRu7aOzUa5ML/kkM8Ne9y3IS+aeYsbhW20WUdv0ifJgdnsxI3D6iJY="
+MONGO_ADDRESS = "127.0.0.1"
+MONGO_PORT = 27017
