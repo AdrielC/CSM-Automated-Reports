b6e930f653ccf1faf847b2109e51e990925aa9f8
Adriel Casellas <adrielcasellas@gmail.com>
2017-10-30 23:11:28 -0600

    a

--- BridgeREST/ClubAttendeeReport.py
+++ BridgeREST/ClubAttendeeReport.py
@@ -10,3 +10,6 @@ def main():
     reportList["2015-2018 Club Event Attendees"] = "2aeeeb2dd7bab015a88858cfb65fb802"
     for reportName, reportId in reportList.items():
         RunReport(reportName, reportName)
+
+if __name__ == "__main__":
+    main()
