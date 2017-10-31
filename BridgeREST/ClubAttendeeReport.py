fb32dfc614cb1c42e76590f36478361e2c728a20
Adriel Casellas <adrielcasellas@gmail.com>
2017-10-31 16:06:37 -0600

    edits

--- BridgeREST/ClubAttendeeReport.py
+++ BridgeREST/ClubAttendeeReport.py
@@ -5,14 +5,19 @@
 import setup
 from reports import *
 
-dataframeOut = []
+def studentNameMerge():
+    print("Lmao")
 
 def main():
+    dataframeOut = []
     reportList = {}
     reportList["2015-2018 Club Event Attendees"] = "2aeeeb2dd7bab015a88858cfb65fb802"
     reportList["2017-2018 Full Student List"] = "e5dcb7ef720881dc8a580753d14d9e84"
     for reportName, reportId in reportList.items():
         dataframeOut.append(RunReport(reportName, reportId, dataframe = True, csv = False))
+
+    print(dataframeOut)
+
 
 if __name__ == "__main__":
     main()
