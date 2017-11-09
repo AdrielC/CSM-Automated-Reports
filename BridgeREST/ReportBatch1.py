b56c45dc1052cab3c30c72a1a1bde75f62174e19
Adriel Casellas <adrielcasellas@gmail.com>
2017-11-09 11:46:56 -0700

    Added ReportBatch1 🅱️

--- BridgeREST/ReportBatch1.py
+++ BridgeREST/ReportBatch1.py
@@ -20,11 +20,13 @@ def main():
     # the report ID from the Bridge(CSM). Use the function GetReportList to see the report IDs.
     # The reports need to be fed into the RunReport as dictionary
     reportList["2015-2018 Club Event Attendees"] = "2aeeeb2dd7bab015a88858cfb65fb802"
-    reportList["2017-2018 Full Student List"] = "e5dcb7ef720881dc8a580753d14d9e84"
+    # reportList["Full Student List **ADRIEL**"] = "e5dcb7ef720881dc8a580753d14d9e84"
+    reportList["Company info sessions **ADRIEL**"] = "b137771b4028f6b311e5dc0ee4ad13cf"
+    reportList["OCR Job Application Count **ADRIEL**"] = "bedde81ee42953863032c71bc630207c"
     for reportName, reportId in reportList.items():
         dataframeOut.append(RunReport(reportName, reportId, dataframe = True, csv = False))
 
-    
+
 
 
 if __name__ == "__main__":
