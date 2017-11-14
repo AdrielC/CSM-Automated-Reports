728712b1893eec6ae2fde21857047f31764484e4
Adriel Casellas <adrielcasellas@gmail.com>
2017-11-14 06:35:50 -0700

    More Reports for PDF

--- BridgeREST/ClubAttendeeReport.py
+++ BridgeREST/ClubAttendeeReport.py
@@ -20,7 +20,9 @@ def main():
     # To run reports individually, you only need to supply the RunReport function with
     # the report ID from the Bridge(CSM). Use the function GetReportList to see the report IDs.
     # The reports need to be fed into the RunReport as dictionary
+    reportList["2013-2018 Club Event Attendees **ADRIEL**"] = "79d7e7e4a0f91c0cb17127f95429fd46"
     reportList["2017-2018 Club Event Attendees"] = "c5237823fed6ede58f19b3e105289803"
+    reportList["Full Student List **ADRIEL**"] = "e5dcb7ef720881dc8a580753d14d9e84"
     for reportName, reportId in reportList.items():
         dataframeOut.append(RunReport(reportName, reportId, dataframe = False, csv = True, directory = directoryOUT))
 
